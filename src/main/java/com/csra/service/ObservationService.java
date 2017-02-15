961df9383394237b6814dd69f1536b8825a6fee0
Steffen Kory <Steffen.Kory@csra.com>
2017-02-14 18:50:35 -0800

    More mapping!

--- src/main/java/com/csra/service/ObservationService.java
+++ src/main/java/com/csra/service/ObservationService.java
@@ -1,9 +1,17 @@
 package com.csra.service;
 
+import ca.uhn.fhir.model.dstu2.composite.CodeableConceptDt;
+import ca.uhn.fhir.model.dstu2.composite.CodingDt;
+import ca.uhn.fhir.model.dstu2.composite.IdentifierDt;
 import ca.uhn.fhir.model.dstu2.composite.PeriodDt;
 import ca.uhn.fhir.model.dstu2.composite.QuantityDt;
+import ca.uhn.fhir.model.dstu2.composite.ResourceReferenceDt;
 import ca.uhn.fhir.model.dstu2.resource.Observation;
+import ca.uhn.fhir.model.dstu2.valueset.ObservationStatusEnum;
+import ca.uhn.fhir.model.primitive.DecimalDt;
 import ca.uhn.hl7v2.HL7Exception;
+import ca.uhn.hl7v2.model.DataTypeException;
+import ca.uhn.hl7v2.model.Type;
 import ca.uhn.hl7v2.model.Varies;
 import ca.uhn.hl7v2.model.v22.datatype.TS;
 import ca.uhn.hl7v2.model.v22.datatype.CE;
@@ -15,15 +23,21 @@ import ca.uhn.hl7v2.model.v22.datatype.ST;
 import ca.uhn.hl7v2.model.v22.datatype.TX;
 import ca.uhn.hl7v2.model.v22.group.ORU_R01_OBSERVATION;
 import ca.uhn.hl7v2.model.v22.group.ORU_R01_ORDER_OBSERVATION;
+import ca.uhn.hl7v2.model.v22.group.ORU_R01_PATIENT_RESULT;
 import ca.uhn.hl7v2.model.v22.message.ORU_R01;
 import ca.uhn.hl7v2.model.v22.segment.MSH;
 import ca.uhn.hl7v2.model.v22.segment.OBX;
+import ca.uhn.hl7v2.model.v22.segment.PID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
+
+import static org.bouncycastle.asn1.x500.style.RFC4519Style.o;
+import static org.bouncycastle.asn1.x500.style.RFC4519Style.st;
 
 /**
  * Created by steffen on 1/21/17.
@@ -35,14 +49,74 @@ public class ObservationService extends RootService {
 
     public Observation toObservation(ORU_R01 oru) {
         Observation observation = new Observation();
-        QuantityDt value = new QuantityDt();
-        OBX obx = oru.getPATIENT_RESULT().getORDER_OBSERVATION().getOBSERVATION().getOBX();
 
-        value.setCode(obx.getObservationValue().getName());
-        value.setUnit(obx.getObservationValue().getData().toString());
-        value.setSystem("");
 
-        observation.setValue(value);
+        PID pid = oru.getPATIENT_RESULT().getPATIENT().getPID();
+        MSH msh = oru.getMSH();
+
+        try {
+            for(ORU_R01_ORDER_OBSERVATION oo : oru.getPATIENT_RESULT().getORDER_OBSERVATIONAll()) {
+                for (ORU_R01_OBSERVATION o : oo.getOBSERVATIONAll()) {
+                    OBX obx = o.getOBX();
+
+                    IdentifierDt identifierDt = new IdentifierDt();
+                    identifierDt.setValue(obx.getObx1_SetIDObservationalSimple().getValue());
+                    observation.getIdentifier().add(identifierDt);
+
+                    CodeableConceptDt code = observation.getCode();
+                    CodingDt coding = new CodingDt();
+                    CE ce = obx.getObservationIdentifier();
+                    coding.setCode(ce.getCe1_Identifier().getValue());
+                    coding.setDisplay(ce.getCe2_Text().getValue());
+                    coding.setSystem(ce.getCe3_NameOfCodingSystem().getValue());
+                    code.addCoding(coding);
+
+                    List<ResourceReferenceDt> performers = observation.getPerformer();
+                    ResourceReferenceDt performer = new ResourceReferenceDt();
+                    performer.setDisplay(obx.getResponsibleObserver().getCn2_FamilyName().getValue());
+                    performer.setReference(obx.getResponsibleObserver().getCn1_IDNumber().getValue());
+                    performers.add(performer);
+
+                    // Not loop enabled
+                    observation.setStatus(ObservationStatusEnum.forCode(obx.getObservationResultStatus().getValue()));
+
+                    ResourceReferenceDt subject = observation.getSubject();
+                    subject.setDisplay(pid.getPatientIDExternalID().getCk1_IDNumber().getValue());
+
+                    PeriodDt period = new PeriodDt();
+                    try {
+                        period.setStartWithSecondsPrecision(obx.getObx14_DateTimeOfTheObservation().getTimeOfAnEvent().getValueAsDate());
+                    } catch (DataTypeException e) {
+                        e.printStackTrace();
+                    }
+
+                    try {
+                        observation.setIssuedWithMillisPrecision(msh.getDateTimeOfMessage().getTimeOfAnEvent().getValueAsDate());
+                    } catch (DataTypeException e) {
+                        e.printStackTrace();
+                    }
+
+                    QuantityDt value = new QuantityDt();
+                    Type type = obx.getObservationValue().getData();
+                    value.setCode(obx.getValueType().getValue());
+                    value.setUnit(obx.getUnits().getCe2_Text().getValue());
+                    if(type instanceof CE) {
+                        try {
+                            CE quantity = (CE) type;
+                            value.setValue(new DecimalDt((quantity.getText().getValue())));
+                        } catch (Exception e) {
+                            value.setValue(0);
+                        }
+                    } else {
+
+                    }
+                    value.setSystem(obx.getUnits().getNameOfCodingSystem().getValue());
+                    observation.setValue(value);
+                }
+            }
+        } catch (HL7Exception e) {
+            e.printStackTrace();
+        }
 
         return observation;
     }
@@ -51,16 +125,21 @@ public class ObservationService extends RootService {
         ORU_R01 message = new ORU_R01();
 
         try {
-            QuantityDt value =  (QuantityDt) observation.getValue();
+            QuantityDt value = (QuantityDt) observation.getValue();
 
             message.initQuickstart("ORU", "R01", "T");
 
             MSH msh = message.getMSH();
+            ORU_R01_PATIENT_RESULT patientResult = message.getPATIENT_RESULT();
 
-            OBX obx = message.getPATIENT_RESULT().getORDER_OBSERVATION().getOBSERVATION(0).getOBX();
+            int i = 0;
+            for(IdentifierDt identifierDt : observation.getIdentifier()) {
+                OBX obx = patientResult.insertORDER_OBSERVATION(i++).getOBSERVATION().getOBX();
+                SI si1 = obx.getSetIDObservationalSimple();
+                si1.setValue(identifierDt.getValue());
+            }
 
-            SI si1 = obx.getSetIDObservationalSimple();
-            si1.setValue(observation.getIdentifier().get(0).getValue());
+            OBX obx = patientResult.getORDER_OBSERVATION().getOBSERVATION(0).getOBX();
 
             ID id2 = obx.getValueType();
             id2.setValue(value.getUnit());
@@ -85,7 +164,12 @@ public class ObservationService extends RootService {
             ce6.getText().setValue(value.getUnit());
 
             ST st7 = obx.getReferencesRange();
-            st7.setValue(observation.getReferenceRange().get(0).getHigh().getValue().toString());
+            try {
+                st7.setValue(observation.getReferenceRange().get(0).getHigh().getValue().toString());
+            } catch (Exception e) {
+                st7.setValue("");
+            }
+
 
             ID[] id8 = obx.getAbnormalFlags();
 
@@ -100,7 +184,11 @@ public class ObservationService extends RootService {
             ST st13 = obx.getUserDefinedAccessChecks();
 
             TS ts14 = obx.getDateTimeOfTheObservation();
-            ts14.getTimeOfAnEvent().setValue(((PeriodDt) observation.getEffective()).getStart().toString());
+            try {
+                ts14.getTimeOfAnEvent().setValue(((PeriodDt) observation.getEffective()).getStart().toString());
+            } catch (Exception e) {
+                ts14.getTimeOfAnEvent().setValue(new Date());
+            }
             ts14.getDegreeOfPrecision().setValue("");
 
             CE ce15 = obx.getProducerSID();
